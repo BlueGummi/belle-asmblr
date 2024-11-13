d8828e1653213f23b826faf9e78ce48296d2b596
gummi <thatstealthyowl@gmail.com>
2024-11-12 22:11:41 -0600

    implementing more tests, testing testing testing

--- tests/basic_tests.rs
+++ tests/basic_tests.rs
@@ -1,10 +1,20 @@
 use belle_asm::*;
 #[test]
 pub fn basic_checks() {
-    let result = encode_instruction(
-        &Token::Ident("mov".to_string()),
-        Some(&Token::Register(0)),
-        Some(&Token::Literal(4)),
-    );
-    assert_eq!(result, -7932);
+    let mut ident = Token::Ident("mov".to_string());
+    let mut op1 = Some(&Token::Register(0));
+    let mut op2 = Some(&Token::Literal(4));
+    let result = encode_instruction(&ident, op1, op2);
+    assert_eq!(result, -0b1111011111100);
+
+    ident = Token::Ident("nop".to_string());
+    let result = encode_instruction(&ident, None, None);
+    assert_eq!(result, 0);
+    let result = encode_instruction(&ident, op1, op2);
+
+    let result = encode_instruction(&ident, op1, op2);
+
+    let result = encode_instruction(&ident, op1, op2);
+
+    let result = encode_instruction(&ident, op1, op2);
 }
